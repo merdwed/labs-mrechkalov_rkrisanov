f2debba71d703142891ae7afaca6746ae517eca5
merdwed <rechkalov_mihail@mail.ru>
2020-10-03 23:46:27 +0300

    move server to diff branch

--- src/main/java/client/ClientNet/Answer.java
+++
@@ -1,9 +0,0 @@
-package client.ClientNet;
-
-import java.io.IOException;
-
-public class Answer {
-    public static void send() throws IOException {
-        Connection.getInstance().getClient().send(PackageOut.getInstance().getBufferOut(),Connection.getInstance().getServerAddress());
-    }
-}
