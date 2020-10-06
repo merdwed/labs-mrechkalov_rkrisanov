c4894be47a17baa711f9aeb04f621451f90af521
merdwed <rechkalov_mihail@mail.ru>
2020-10-06 07:51:27 +0300

    check password

--- src/main/java/server/Threading/ExecuteForkJoinTask.java
+++ src/main/java/server/Threading/ExecuteForkJoinTask.java
@@ -1,11 +1,15 @@
 package server.Threading;
 
+import server.DataBase.DataBase;
 import server.ServerNet.Answer;
 import server.ServerNet.Request;
 import server.ServerNet.ServerCommandFactory;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.concurrent.RecursiveTask;
+
+import DataClasses.CommandTypeUtils.CommandType;
 
 public class ExecuteForkJoinTask extends RecursiveTask<Answer> {
     Request request;
@@ -18,12 +22,22 @@ public class ExecuteForkJoinTask extends RecursiveTask<Answer> {
 
     @Override
     public Answer compute() {
-        synchronized(request) {System.out.println("execute runs " + request.getAccount().getLogin());};
+        synchronized (request) {
+            System.out.println("execute runs " + request.getAccount().getLogin());
+        }
+        ;
         try {
-            synchronized(request) {
-                synchronized (ServerCommandFactory.class){ServerCommandFactory.createCommand(request.getCommandType()).execute(request, answer);}
+            synchronized (request) {
+                synchronized (ServerCommandFactory.class) {
+                    if (DataBase.getInstance().checkAccount(request.getAccount())
+                            || request.getCommandType().equals(CommandType.CREATE_ACCOUNT))
+                        ServerCommandFactory.createCommand(request.getCommandType()).execute(request, answer);
+                }
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         synchronized(request) {System.out.println("execute ends " + request.getAccount().getLogin());};
