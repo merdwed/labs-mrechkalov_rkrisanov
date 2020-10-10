89bfa450e421728ab045fd6b58f82c892a5710c7
merdwed <rechkalov_mihail@mail.ru>
2020-10-10 16:58:33 +0300

    fix comparator

--- src/main/java/DataClasses/Comparators/TicketTypeComparator.java
+++ src/main/java/DataClasses/Comparators/TicketTypeComparator.java
@@ -7,6 +7,9 @@ import DataClasses.Ticket;
 public class TicketTypeComparator implements Comparator<Ticket> {
     @Override
     public int compare(Ticket o1, Ticket o2) {
+        if(o1.getType()==null && o2.getType()==null)return 0;
+        if(o1.getType()==null)return -1;
+        if(o2.getType()==null)return 1;
         return o1.getType().compareTo(o2.getType());
     }
 }
